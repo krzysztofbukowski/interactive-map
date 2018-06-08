7c18be3a025162b0ca79b14852db79d1ae2d9fe9
Krzysztof Bukowski <krzysztof.bukowski@schibsted.com>
2018-06-08 09:50:57 +0200

    Styled the map

--- src/App.tsx
+++ src/App.tsx
@@ -11,46 +11,77 @@ interface IAppProps {
   height: number;
 }
 
+interface IAppState {
+  currentArea: string;
+}
+
+
+interface IValnattTopoFeature {
+  features?: any,
+  properties: {
+    LAN: string,
+    LAN_NAMN: string,
+    ID: string
+  }
+}
+
+
 declare type TopoFeature =
   FeatureCollection
   & Feature<Point, GeoJsonProperties>;
 
-class App extends React.Component<IAppProps, {}> {
+class App extends React.Component<IAppProps, IAppState> {
   private ref: SVGSVGElement;
 
 
   constructor(props: IAppProps) {
     super(props);
 
-    console.log(props);
+
+    this.state = {
+      currentArea: 'n/a'
+    };
   }
 
   public componentDidMount() {
     const svg = d3.select(this.ref);
     const path = d3.geoPath();
+    // const swedenCenter = ;
+
+
+    const swedenProjection = d3.geoConicEqualArea()
+      .scale(this.props.width * 3.5)
+      .center([15.1, 61.6])
+      .translate([this.props.width / 2, this.props.height / 2]);
+
+    const swedenProjectionPath = d3.geoPath(swedenProjection);
+
 
     fetch('/sweden.json')
       .then((response: any) => response.json())
       .then((us: any) => {
         console.log(us);
-        const feature = topojson.feature(us, us.objects.national_250) as TopoFeature;
+        const feature = topojson.feature(us, us.objects.national_250) as IValnattTopoFeature;
 
         svg.append('g')
           .attr('class', 'counties')
           .selectAll('path')
           .data(feature.features)
           .enter().append('path')
-          .attr('d', path);
-
-        // the following block is new, adding JS events
-        let hoverEnabled = false;
-        svg.on('mousedown', (x: any) => hoverEnabled = true)
-          .on('mouseup', (x: any) => hoverEnabled = false);
+          .attr('d', (item: TopoFeature) => {
+            return swedenProjectionPath(item);
+          });
 
         const paths = svg.selectAll('.counties path');
 
-        paths.on('mouseover', function () {
-          (this as Element).classList.add('hovered');
+        paths.on('mousedown', (clickedPath: IValnattTopoFeature) => {
+          this.setState({
+            currentArea: clickedPath.properties.LAN_NAMN
+          })
+        }).on('mouseover', function(item: IValnattTopoFeature) {
+          (this as Element).classList.add('selectedArea');
+        }).on('mouseout', function(item: IValnattTopoFeature) {
+          (this as Element).classList.remove('selectedArea');
         });
 
         const pathString: string = path(
@@ -66,25 +97,17 @@ class App extends React.Component<IAppProps, {}> {
   public render() {
     return (
       <div>
+        <h1>Sweden areas</h1>
+        <h2>{this.state.currentArea}</h2>
         <svg
           className="container"
           ref={(ref: SVGSVGElement) => this.ref = ref}
           width={this.props.width}
           height={this.props.height}
         />
-        <button type="button" className="button"
-                onClick={this.handleClick}>Reset
-        </button>
       </div>
     );
   }
-
-  private handleClick = () => {
-    const svg = d3.select(this.ref);
-    svg.selectAll('path').each(function() {
-      (this as Element).classList.remove('hovered');
-    });
-  };
 }
 
 export default App;
