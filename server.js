e033f709cf3cf4e9a6d724b2ed458756aab5e905
Krzysztof Bukowski <krzysztof.bukowski@schibsted.com>
2018-06-11 09:57:33 +0200

    Use separate declarations

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express'),
-      path = require('path');
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const maxAge = 1000 * 3600 * 24 * 7;
