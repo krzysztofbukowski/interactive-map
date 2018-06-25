a6cd7001e1a08be56cbad5005a34e2c79e902aad
Krzysztof Bukowski <krzysztof.bukowski@schibsted.com>
2018-06-25 13:57:08 +0200

    Refactored map loader

--- src/components/Map/MapDataLoader.tsx
+++ src/components/Map/MapDataLoader.tsx
@@ -2,6 +2,13 @@ import * as React from 'react';
 import utils from '../../utils/utils';
 import { ITopology } from './Map';
 
+enum MapDataLevels {
+  NATIONAL = 100,
+  COUNTY = 10,
+  MUNICIPALITY = 1,
+  DISTRICT = 1
+}
+
 interface IMapLoaderProps {
   dataSourceHost: string;
   areaCode: string;
@@ -10,6 +17,8 @@ interface IMapLoaderProps {
   delay: number
 }
 
+const AREA_CODE_NATIONAL = 'national';
+
 class MapDataLoader extends React.Component<IMapLoaderProps> {
 
   public render() {
@@ -17,11 +26,10 @@ class MapDataLoader extends React.Component<IMapLoaderProps> {
   }
 
   public componentDidMount() {
-    this.fetchDataForArea('national', 100)
-      .then(this.nationalDataFetched.bind(this))
-      .then(this.tryToFetchLanData.bind(this))
-      .then(this.tryToFetchKommunData.bind(this))
-      .then(this.tryToFetchValkretsData.bind(this))
+    this.tryToFetchNationalData()
+      .then(this.tryToFetchCountyData)
+      .then(this.tryToFetchMunicipalityData)
+      .then(this.tryToFetchValkretsData)
       .catch(this.props.onDataLoadCompleted);
   }
 
@@ -31,97 +39,91 @@ class MapDataLoader extends React.Component<IMapLoaderProps> {
     }
 
     const level = this.resolveLevel(this.props.areaCode);
-    this.fetchDataForArea(this.props.areaCode, level);
+    this.fetchDataForArea(this.props.areaCode, level, 0);
   }
 
-  private fetchDataForArea(areaCode: string, level: number): Promise<any> {
-    const url = `${this.props.dataSourceHost}/api/topojson/val2014/${areaCode}/${level}`;
-    return fetch(url)
-      .then((response: any) => response.json())
-      .then((topology: ITopology) => {
-        if (topology.objects) {
-          this.props.onDataLoaded(topology, areaCode);
+  private fetchDataForArea = (areaCode: string, level: number, delay: number): Promise<any> => {
+    return utils.delay(delay).then(() => {
+      const url = `${this.props.dataSourceHost}/api/topojson/val2014/${areaCode}/${level}`;
+
+      return fetch(url)
+        .then((response: any) => response.json())
+        .then((topology: ITopology) => {
+          if (topology.objects) {
+            this.props.onDataLoaded(topology, areaCode || '');
+          }
+
+          return topology;
+        }).catch(e => {
+          console.info(`Failed loading data from ${url}`);
+          console.error(e);
+        });
+    })
+  }
+
+  private tryToFetchNationalData = (): Promise<string | undefined> => {
+    return this.fetchDataForArea(AREA_CODE_NATIONAL, MapDataLevels.NATIONAL, this.props.delay)
+      .then((data: ITopology) => {
+        if (this.props.areaCode === AREA_CODE_NATIONAL) {
+          return Promise.reject(undefined);
         }
 
-        return topology;
-      }).catch(e => {
-        console.info(`Failed loading data from ${url}`);
-        console.error(e);
-      });
-  }
+        const lanCode = this.props.areaCode.substr(0, 2);
 
-  private nationalDataFetched(topolog: ITopology): Promise<string | undefined> {
-    if (this.props.areaCode === 'national') {
-      return Promise.reject(undefined);
-    }
-    return Promise.resolve(this.props.areaCode.substr(0, 2));
-  }
-
-  private tryToFetchLanData(lanCode: string) {
-    return utils.delay(this.props.delay)
-      .then(() => {
-        return this.fetchDataForArea(lanCode, 10)
-          .then((data: ITopology) => {
-            const kommunCode = this.props.areaCode.substr(0, 4);
-
-            if (kommunCode.length === 4) {
-              return Promise.resolve(kommunCode)
-            } else {
-              return Promise.reject(undefined);
-            }
-          });
-      })
-      .catch(() => {
-        return Promise.reject(undefined);
+        if (lanCode.length === 2) {
+          return Promise.resolve(lanCode)
+        } else {
+          return Promise.reject(undefined);
+        }
       });
   };
 
-  private tryToFetchKommunData(kommunCode: string): Promise<string | undefined> {
-    return utils.delay(this.props.delay)
-      .then(() => {
-        return this.fetchDataForArea(kommunCode, 1)
-        .then((data: ITopology) => {
-          const valkretsCode = this.props.areaCode.substr(0, 6);
+  private tryToFetchCountyData = (countyCode: string): Promise<string | undefined> => {
+    return this.fetchDataForArea(countyCode, MapDataLevels.COUNTY, this.props.delay)
+      .then((data: ITopology) => {
+        const municipalityCode = this.props.areaCode.substr(0, 4);
 
-          if (valkretsCode.length === 6) {
-            return Promise.resolve(valkretsCode)
-          } else {
-            return Promise.reject(undefined);
-          }
+        if (municipalityCode.length === 4) {
+          return Promise.resolve(municipalityCode)
+        } else {
           return Promise.reject(undefined);
-        })
-      })
-      .catch(() => {
-        return Promise.reject(undefined);
+        }
+      });
+  };
+
+  private tryToFetchMunicipalityData = (municipalityCode: string): Promise<string | undefined> => {
+    return this.fetchDataForArea(municipalityCode, MapDataLevels.MUNICIPALITY, this.props.delay)
+      .then((data: ITopology) => {
+        const districtCode = this.props.areaCode.substr(0, 6);
+
+        if (districtCode.length === 6) {
+          return Promise.resolve(districtCode)
+        } else {
+          return Promise.reject(undefined);
+        }
       });
   }
 
-  private tryToFetchValkretsData(kommunCode: string): Promise<string | undefined> {
-    return utils.delay(this.props.delay)
-      .then(() => {
-        return this.fetchDataForArea(kommunCode, 1)
-        .then((data: ITopology) => {
-          /**
-           * Handle loading data for valdistrikt
-           */
-          return Promise.reject(undefined);
-        })
-      })
-      .catch(() => {
+  private tryToFetchValkretsData = (districCode: string): Promise<string | undefined> => {
+    return this.fetchDataForArea(districCode, MapDataLevels.DISTRICT, this.props.delay)
+      .then((data: ITopology) => {
+        /**
+         * Handle loading data for valdistrikt
+         */
         return Promise.reject(undefined);
-      });
-  }  
+      })
+  }
 
   private resolveLevel(feature: string) {
     switch (feature.length) {
       case 2:
-        return 10;
+        return MapDataLevels.COUNTY;
       case 4:
-        return 1;
+        return MapDataLevels.MUNICIPALITY;
       case 6:
-        return 1;
+        return MapDataLevels.DISTRICT;
       default:
-        return 1;
+        return MapDataLevels.NATIONAL;
     }
   }
 }
