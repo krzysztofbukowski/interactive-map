eb54f5daeafa7d2637dcab03bc8a8576d4eb5831
Krzysztof Bukowski <krzysztof.bukowski@schibsted.com>
2018-10-13 22:53:13 +0200

    Removed router and tv container

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import * as React from 'react';
-import { Route } from 'react-router';
 
 import './App.css';
 import DefaultVersionContainer from './containers/DefaultVersionContainer';
-import TvVersionContainer from './containers/TvVersionContainer';
 
 class App extends React.Component {
   public render() {
     return (
-      <>
-        <Route
-          path="/tv"
-          isExact={true}
-          component={TvVersionContainer}
-        />
-        <Route
-          path="/"
-          isExact={true}
-          component={DefaultVersionContainer}
-        />
-      </>
+      <DefaultVersionContainer />
     )
   }
 }
